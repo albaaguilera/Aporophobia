14c5c536df18b1655fc93699e9f0666384412432
Nieves Montes <mnm24896@gmail.com>
2023-05-17 14:31:48 +0200

    fix submission scripts

--- run_model.py
+++ run_model.py
@@ -4,12 +4,14 @@ from itertools import chain, combinations
 from model import CityModel, lista_distritos, dummy_norms
 
 parser = argparse.ArgumentParser(description="Run a sample of models.")
+parser.add_argument('-P', default='.', type=str, help="Path to save results")
 parser.add_argument('-N', default=100, type=int, help="number of agents")
 parser.add_argument('-T', default=100, type=int, help="number of time steps")
 parser.add_argument('-M', default=100, type=int, help="number of samples per \
                     norm configuration")
 
 args = parser.parse_args()
+path = args.P
 N, T, M = args.N, args.T, args.M
 
 
@@ -30,8 +32,8 @@ def run_model(I):
             run_id = f"{process_id}_{index}_{i}"
             model_vars_df = barcelona.datacollector.get_model_vars_dataframe()
             agent_vars_df = barcelona.datacollector.get_agent_vars_dataframe()
-            model_vars_df.to_csv(f"results/model_vars_{run_id}.csv", sep=';')
-            agent_vars_df.to_csv(f"results/agent_vars_{run_id}.csv", sep=';')
+            model_vars_df.to_csv(f"{path}/model_vars_{run_id}.csv", sep=';')
+            agent_vars_df.to_csv(f"{path}/agent_vars_{run_id}.csv", sep=';')
 
 
 if __name__ == '__main__':
