a80fd839f4c327255abc8d982e6b473eeab82ba3
Nieves Montes <mnm24896@gmail.com>
2023-05-17 12:27:51 +0200

    fix submission script

--- run_model.sh
+++ run_model.sh
@@ -9,14 +9,9 @@
 #SBATCH --error=/home/nmontes/logs/%x-%j.err
 #SBATCH --mail-user=nmontes@iiia.csic.es
 
-# You must carefully match tasks, cpus, nodes,
-# and cpus-per-task for your job. See docs.
-#SBATCH --cpus-per-task=20
-#SBATCH --tasks-per-node=1
-
 for i in {1..$SLURM_JOB_NUM_NODES}
 do
-    srun --nodes 1 --ntasks 1 --cpus-per-task 20 --exclusive /home/nmontes/.conda/envs/aporo/bin/python run_model.py &
+    srun --nodes 1 --ntasks 1 --exclusive /home/nmontes/.conda/envs/aporo/bin/python run_model.py &
 done
 
 wait
